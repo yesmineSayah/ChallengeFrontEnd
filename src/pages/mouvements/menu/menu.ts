9e04c03f2b3a2542eb7dfbc1faaa8f7931aef0f0
Khaled Romdhane <heiyukidev@gmail.com>
2017-02-11 20:19:45 +0100

    Mouvement Menu

--- src/pages/mouvements/menu/menu.ts
+++ src/pages/mouvements/menu/menu.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MouvementDetails } from '../details/details';
-
+import { MouvementMy } from '../myMouvements/my';
+import { MouvementPage } from '../index/index';
 @Component({
   selector: 'app-mouvementmenu',
   templateUrl: 'menu.html'
@@ -13,5 +14,17 @@ export class MouvementMenu {
 
   }
   ngOnInit() {
+  }
+
+  indexRoute(){
+    this.navCtrl.setRoot(MouvementPage,null,{animate:true,direction:"forward"});
+  }
+
+  myRoute(){
+    this.navCtrl.setRoot(MouvementMy,null,{animate:true,direction:"forward"});
+  }
+
+  backRoute(){
+    this.navCtrl.pop();
   }
 }
--- src/pages/mouvements/myMouvements/my.ts
+++ src/pages/mouvements/myMouvements/my.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MouvementsService } from '../../../services/mouvements.service';
+import { MouvementMenu } from '../menu/menu';
 @Component({
   selector: 'app-mouvementmy',
-  templateUrl: 'my.html'
+  templateUrl: 'my.html',
+  providers: [MouvementsService]
 })
 export class MouvementMy {
-
-  constructor(public navCtrl: NavController) {
+items: any;
+  constructor(public navCtrl: NavController, private service: MouvementsService) {
 
   }
   ngOnInit(){
-
+    this.service.getMouvements().subscribe(data => {
+      this.items = data;
+    });
   }
-
+  menuRoute(){
+    this.navCtrl.push(MouvementMenu);
+  }
 }
